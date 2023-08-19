d696ba8a34ef9f2260653b5d72dcf2c453598119
Himanshu Saraswat <31274229+himanshusaraswat@users.noreply.github.com>
2023-08-19 15:14:52 +0530

    test deploy

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -8,8 +8,8 @@ import { fastifyMongodb, FastifyMongodbOptions } from '@fastify/mongodb'
  */
 export default fp<FastifyMongodbOptions>(async (fastify) => {
   fastify.register(fastifyMongodb, {
-    url:'mongodb+srv://prj-dev-meraki-gallery-read:lnnqCFZ5BhodoA8Z@cluster0.l7v9qsp.mongodb.net/?retryWrites=true&w=majority',
+    url:`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l7v9qsp.mongodb.net/?retryWrites=true&w=majority`,
     forceClose: true,
-    database: 'prj-dev-gallery'
+    database: process.env.DB_NAME
   })
 })
